56eeaa4dabfb0057474fdf2288c42e3ee3ef26f0
sabelogumede <Iamsabelogumede@gmail.com>
2018-09-17 12:47:52 +0200

    master file

---
+++ images/Logoa_files/BizibleAcct.js
@@ -0,0 +1,12 @@
+(function () {
+    BizTrackingA.GoAccount({
+        webToLeadField: '',
+        chatEnabled: 'true' === 'true',
+        XUserId: '952e0737a9ee4b798972870db52513ff',
+        formProviderEnabled: 'true' === 'true',
+        attach_secure_forms: 'true' === 'true',
+        viewThroughEnabled: 'true' === 'true',
+        interceptAjax: 'true' === 'true'
+    });
+})();
+;
